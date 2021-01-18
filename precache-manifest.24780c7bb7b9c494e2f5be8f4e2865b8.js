00057629b8bc471a49df182e67ebe1fb261bdf61
Yukai <yukaiy@cs.washington.edu>
2021-01-18 04:37:30 -0800

    Updates

---
+++ precache-manifest.24780c7bb7b9c494e2f5be8f4e2865b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba04445771425a5f5b711f18b58fe29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2d7c9611ecee92454f4",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "12289687e1e7c9b0cafe",
+    "url": "/static/js/2.b4dc074a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.b4dc074a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d7c9611ecee92454f4",
+    "url": "/static/js/main.449b237c.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1394e306fcb6920c435d0e74f17d8953",
+    "url": "/static/media/profilePic.1394e306.jpg"
+  },
+  {
+    "revision": "ef783e01f1d01f03193523542551b53e",
+    "url": "/static/media/resume.ef783e01.pdf"
+  }
+]);
