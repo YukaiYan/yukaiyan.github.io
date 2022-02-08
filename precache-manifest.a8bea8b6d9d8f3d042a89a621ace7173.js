fe9721b15d8c368f0b97d60319a5dd9eef9a7baf
Yukai <yukaiy@cs.washington.edu>
2022-02-08 14:52:40 -0800

    First Commit

---
+++ precache-manifest.a8bea8b6d9d8f3d042a89a621ace7173.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adf8278649152939a1da49dadde2db1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e76f85114248c7eefc6b",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "00299f7a191fc60be84f",
+    "url": "/static/css/main.8a7dfedb.chunk.css"
+  },
+  {
+    "revision": "e76f85114248c7eefc6b",
+    "url": "/static/js/2.05fed1bd.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/static/js/2.05fed1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00299f7a191fc60be84f",
+    "url": "/static/js/main.11c6e1e8.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "4b40cd7123970023792bbb3a0a6f20a6",
+    "url": "/static/media/Allen-logo.4b40cd71.jpg"
+  },
+  {
+    "revision": "5438f770dc6a97bbf564e41db9ff37e9",
+    "url": "/static/media/Google-logo.5438f770.png"
+  },
+  {
+    "revision": "1394e306fcb6920c435d0e74f17d8953",
+    "url": "/static/media/profilePic.1394e306.jpg"
+  },
+  {
+    "revision": "baf62d83769d8eff27afacd11268f4a1",
+    "url": "/static/media/resume.baf62d83.pdf"
+  }
+]);
